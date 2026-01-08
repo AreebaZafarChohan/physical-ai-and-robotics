870f2b99b313bba98932143a947e634162817717
AreebaZafarChohan <areebazafar715@gmail.com>
2026-01-08 17:33:29 +0500

    Fix merge conflict in database.py

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-from sqlmodel import Session, create_engine, SQLModel
-from dotenv import load_dotenv
-import os
-from pathlib import Path
-
-env_path = Path(__file__).resolve().parents[1] / ".env"
-load_dotenv(dotenv_path=env_path)
-
-DATABASE_URL = os.getenv("NEON_DATABASE_URL")
-
-if not DATABASE_URL:
-    raise ValueError("NEON_DATABASE_URL environment variable is not set.")
-
-engine = create_engine(DATABASE_URL, echo=True) 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-def get_session():
-    with Session(engine) as session:
-        yield session
-=======
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
@@ -47,4 +25,3 @@ async def get_session() -> AsyncSession:
     )
     async with async_session() as session:
         yield session
->>>>>>> 012-docusaurus-i18n-urdu
